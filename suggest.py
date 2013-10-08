8d0a3288aaece6c1ae9f7dea323cf705a6132a6f
wklken <ken@Luna-mac.local>
2013-10-08 11:48:54 +0800

    优化部分代码

--- suggest.py
+++ suggest.py
@@ -14,11 +14,8 @@
 @TODO:
     test case
 
-    返回leaf node放在result里面，并且里面有cache，这样合适？
 """
-
 #这是实现cache的一种方式，也可以使用redis/memcached在外部做缓存
-
 #一旦打开，search时会对每个节点做cache，当增加删除节点时，其路径上的cache会被清除,搜索时间降低了一个数量级
 #代价：内存消耗, 不需要时可以关闭,或者通过CACHED_THREHOLD调整缓存数量
 CACHED = True
@@ -26,11 +23,13 @@ CACHED = True
 #注意，CACHED_SIZE >= search中的limit，保证search从缓存能获取到足够多的结果
 CACHED_SIZE = 10
 #被搜索超过多少次后才加入缓存
-CACHED_THREHOLD = 1
+CACHED_THREHOLD = 10
 
 ############### start ######################
 class PuppetNode(object):
-    pass
+    def __str__(self):
+        s = ["%s:%s" % (field, getattr(self,field)) for field in Node.get_puppet_fields()]
+        return "<PuppetNode %s>" % ','.join(s)
 
 
 class Node(dict):
@@ -47,13 +46,15 @@ class Node(dict):
         #节点前缀搜索次数，可以用于搜索query数据分析
         self.search_count = 0
 
+
     @staticmethod
     def get_puppet_fields():
         """
-        傀儡节点拷贝的项
+        傀儡节点拷贝的项,用来返回，以及缓存的
         NOTICE:可以加入其他属性，例如图标，分类等在展示时需要用到的，根据需要修改数据文件格式和build方法
         """
         return ['weight']
+
 
     def make_puppet_node(self):
         """
@@ -66,7 +67,7 @@ class Node(dict):
 
 
     def __str__(self):
-        return '<Node %s %s> %s' % (self.key, self.is_leaf, self.items())
+        return '<Node key:%s is_leaf:%s> subnodes: %s' % (self.key, self.is_leaf, self.items())
 
 
     def add_subnode(self, node):
@@ -110,7 +111,7 @@ class Node(dict):
 
 def depth_walk(node):
     """
-    递归，深度优先遍历,返回每个节点所代表的key以及叶子节点的权重
+    递归，深度优先遍历一个节点,返回每个节点所代表的key以及所有关键字节点(叶节点)
     """
     result = []
     if node.is_leaf:
@@ -119,34 +120,33 @@ def depth_walk(node):
     if node.has_subnode():
         for k in node.iterkeys():
             s = depth_walk(node.get(k))
-            result.extend([(k+name, n) for name,n in s])
+            result.extend([(k + subkey, pnode) for subkey, pnode in s])
         return result
     else:
         return [('', node.make_puppet_node())]
 
 
-def search(node, prefix, limit=None):
+def search(node, prefix, limit=None, is_case_sensitive=True):
     """
-    搜索一个前缀下的所有单词列表 递归式
+    搜索一个前缀下的所有单词列表 递归
     """
     node = node.get_top_node(prefix)
+
+    #如果找不到前缀节点，代表匹配失败，返回空
     if node is None:
         return []
 
+    #搜索次数递增
     node.search_count += 1
 
     if CACHED and node.cache:
-        print '...................read from cache'
         return node.cache[:limit] if limit is not None else node.cache
 
-    result = []
+    result = [(prefix + subkey, pnode) for subkey, pnode in depth_walk(node)]
 
-    result.extend([(prefix + i, n) for i, n in depth_walk(node)])
-
-    result.sort(key=lambda x: -x[1].weight)
+    result.sort(key=lambda x: x[1].weight, reverse=True)
 
     if CACHED and node.search_count >= CACHED_THREHOLD:
-        print '...................write to cache'
         node.cache = result[:CACHED_SIZE]
 
     return result[:limit] if limit is not None else result
@@ -200,12 +200,12 @@ def delete(node, keyword, judge_leaf=False):
         if not top_node.is_leaf:
             return
 
-    #存在子节点，去除其标志 done
     if top_node.has_subnode():
+        #存在子节点，去除其标志 done
         top_node.is_leaf = False
         return
-    #不存在子节点，逐层检查删除节点
     else:
+        #不存在子节点，逐层检查删除节点
         this_node = top_node
 
         prefix = keyword[:-1]
@@ -238,102 +238,13 @@ def build(file_path, is_case_sensitive=False):
 
 
 if __name__ == '__main__':
-    print "TRACK ================= test 1"
-    node = Node("")
-
-    hn = Node("h")
-    node.add_subnode(hn)
-
-    an = Node("a")
-    en = Node("e", is_leaf=True)
-    hn.add_subnode(an)
-    hn.add_subnode(en)
-
-    ln = Node("l")
-    an.add_subnode(ln)
-
-    on = Node("o")
-    ln.add_subnode(on)
-
-    ln1 = Node("l")
-    rn = Node("r", is_leaf=True)
-    en.add_subnode(ln1)
-    en.add_subnode(rn)
-
-    ln2 = Node("l")
-    an2 = Node("a")
-    ln1.add_subnode(ln2)
-    ln1.add_subnode(an2)
-
-    on2 = Node("o", is_leaf=True)
-    ln2.add_subnode(on2)
-    bn = Node("b", is_leaf=True)
-    an2.add_subnode(bn)
-
-    on1 = Node("o", is_leaf=True)
-    rn.add_subnode(on1)
-
-    add(node, 'hec')
-    add(node, 'helac')
-    add(node, 'hec')
-
-    #print get_top_node(node, 'he')
-    print search(node, 'he')
-
-    print "TRACK ================= test11"
-
     n = Node("")
-    add(n, u'h')
     add(n, u'he')
     add(n, u'her')
 
-    print 'test_get top node'
-    print n.get_top_node(u'her')
     print n.get_top_node(u'he')
-    print n.get_top_node(u'h')
 
-    print "TRACK ================= test2"
-
-    n = build('./game_info')
-    for v,w in search(n, u'植物'):
-        print v, w.weight
-    print 'len', len(search(n, u'植物'))
-
-    import sys
-    print 'total size:', sys.getsizeof(n)
-
-    for v,w in search(n, u'植物'):
-        print v, w.weight
-    search(n, u'植物')
-    print 'len', len(search(n, u'植物'))
-
-    import time
-    b = time.clock()
-    search(n, u'植物')
-    print time.clock() - b
-
-    print "TRACK ================= test3"
-
-    n = Node("")
-    add(n, u'h')
-    add(n, u'he')
-    add(n, u'her')
-
-    print 'before delete', n
-    for v, w in search(n, u'h'):
-        print v, w
-
-    print "TRACK ================= delete begin"
-    delete(n, u'he')
-
-    print 'after delete', n, n is None
-    for v, w in search(n, u'h'):
-        print v, w.weight
-
-    #print 'delete h-done he her heer'
-    #print "TRACK ================= after delete"
-
-    #for v, w in search(n, u'he'):
-        #print v, w
+    for key, pnode in search(n, u'h'):
+        print key, pnode
 
 
